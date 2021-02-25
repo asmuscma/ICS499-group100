168dc126a4bbaa0477ff123f7902ae058df33a38
GoodnewsSama <77777563+GoodnewsSama@users.noreply.github.com>
2021-02-24 23:26:36 -0600

    created ballot class

---
+++ src/main/java/com/group100/VotingApp/data/entity/Ballot.java
@@ -0,0 +1,33 @@
+package com.group100.VotingApp.data.entity;
+
+public class Ballot {
+	private int userID;
+	private int candidateID;
+	private String Office;
+	public Ballot(int userID, int candidateID, String office) {
+		super();
+		this.userID = userID;
+		this.candidateID = candidateID;
+		Office = office;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public int getCandidateID() {
+		return candidateID;
+	}
+	public void setCandidateID(int candidateID) {
+		this.candidateID = candidateID;
+	}
+	public String getOffice() {
+		return Office;
+	}
+	public void setOffice(String office) {
+		Office = office;
+	}
+	
+
+}
