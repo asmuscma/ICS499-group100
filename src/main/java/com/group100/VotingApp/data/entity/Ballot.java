60733cfe6ddfa413819d96ec9287936e88bff6d4
PopcornTheBearCat <54016398+PopcornTheBearCat@users.noreply.github.com>
2021-02-25 16:47:55 -0600

    Added Candidate Repository

--- src/main/java/com/group100/VotingApp/data/entity/Ballot.java
+++ src/main/java/com/group100/VotingApp/data/entity/Ballot.java
@@ -1,42 +1,50 @@
 package com.group100.VotingApp.data.entity;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 @Entity
-@Table(name="Ballot")
+@Table(name = "Ballot")
 public class Ballot {
 	@Id
-	@Column(name="userID")
+	@Column(name = "userID")
 	private int userID;
-	@Column(name="Race")
-	private race Race;
-	@Column(name="office")
+	@Column(name = "Race")
+	private Race race;
+	@Column(name = "office")
 	private String Office;
-	public Ballot(int userID, int candidateID, String office) {
+
+	public Ballot(int userID, int candidateID, String office, Race race) {
 		super();
 		this.userID = userID;
-		this.candidateID = candidateID;
+		this.race = race;
 		Office = office;
 	}
+
 	public int getUserID() {
 		return userID;
 	}
+
 	public void setUserID(int userID) {
 		this.userID = userID;
 	}
-	public int getCandidateID() {
-		return candidateID;
+
+	public Race getRace() {
+		return race;
 	}
-	public void setCandidateID(int candidateID) {
-		this.candidateID = candidateID;
+
+	public void setRace(Race race) {
+		this.race = race;
 	}
+
 	public String getOffice() {
 		return Office;
 	}
+
 	public void setOffice(String office) {
 		Office = office;
 	}
-	
 
 }
