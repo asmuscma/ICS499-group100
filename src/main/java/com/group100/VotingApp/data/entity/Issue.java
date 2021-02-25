6f3c0fe3ffd399e20df77380817fcae5abae83df
GoodnewsSama <77777563+GoodnewsSama@users.noreply.github.com>
2021-02-25 01:32:03 -0600

    added annotation to Issue, unsure whether to add a @column for opinions

--- src/main/java/com/group100/VotingApp/data/entity/Issue.java
+++ src/main/java/com/group100/VotingApp/data/entity/Issue.java
@@ -1,5 +1,8 @@
 package com.group100.VotingApp.data.entity;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -7,8 +10,10 @@ import java.util.ArrayList;
  * An object that represents a political issue which different users,
  * candidates, and parties will have different opinions on.
  */
+@Entity
+@Table(name="Issue")
 public class Issue {
-
+	@Column(name="topic")
 	String topic;
 	List<String> opinions = new ArrayList<String>();
 	public Issue(String topic, List<String> opinions) {
