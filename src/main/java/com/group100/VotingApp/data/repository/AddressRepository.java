b0edf122ded21d40cc9fb94cc82023c6954d2c0d
asmuscma <asmuscma@gmail.com>
2021-03-16 17:33:32 -0500

    Created AddressRepository.java and AddressController.java

---
+++ src/main/java/com/group100/VotingApp/data/repository/AddressRepository.java
@@ -0,0 +1,11 @@
+package com.group100.VotingApp.data.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.group100.VotingApp.data.entity.Address;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
