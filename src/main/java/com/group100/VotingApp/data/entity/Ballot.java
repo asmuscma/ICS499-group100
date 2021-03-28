ea5acdeec3105d1bd4880c36bb66b5570d060a2e
PopcornTheBearCat <54016398+PopcornTheBearCat@users.noreply.github.com>
2021-03-28 18:09:29 -0500

    Ballot added one to one relationship with user

--- src/main/java/com/group100/VotingApp/data/entity/Ballot.java
+++ src/main/java/com/group100/VotingApp/data/entity/Ballot.java
@@ -1,28 +1,23 @@
 package com.group100.VotingApp.data.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ballot extends Poll {
-	private long userId;
+	@OneToOne
+	private User user;
 	private long candidateId;
 	private String office;
+
 	public Ballot() {
-		
+
 	}
-	public Ballot(String question, String answer, long userId, long candidateId, String office) {
+
+	public Ballot(String question, String answer, long candidateId, String office) {
 		super(question, answer);
-		this.userId = userId;
 		this.candidateId = candidateId;
 		this.office = office;
-	}
-
-	public long getUserId() {
-		return userId;
-	}
-
-	public void setUserId(int userId) {
-		this.userId = userId;
 	}
 
 	public long getCandidateId() {
