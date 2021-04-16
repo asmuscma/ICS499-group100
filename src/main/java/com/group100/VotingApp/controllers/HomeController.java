cd7dd7222469339f3667e928fc5a2f4a43abab36
asmuscma <asmuscma@gmail.com>
2021-04-15 19:38:57 -0500

    HomeController returns ModelAndView

--- src/main/java/com/group100/VotingApp/controllers/HomeController.java
+++ src/main/java/com/group100/VotingApp/controllers/HomeController.java
@@ -2,12 +2,15 @@ package com.group100.VotingApp.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HomeController {
 	
-	@GetMapping("/Home")
-	public String home() {
-	return "index";
+	@GetMapping("/")
+	public ModelAndView home() {
+		ModelAndView view = new ModelAndView();
+		view.setViewName("candidate");
+	    return view;
 	}
 }
