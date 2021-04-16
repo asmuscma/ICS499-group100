0208c62a3f5c0eba3cf8f303f9da11a0f79d7076
GoodnewsSama <77777563+GoodnewsSama@users.noreply.github.com>
2021-04-16 13:33:40 -0500

    updated HomeController, added templates folder
    in the templates folder is now the index file and it's css and images.

--- src/main/java/com/group100/VotingApp/controllers/HomeController.java
+++ src/main/java/com/group100/VotingApp/controllers/HomeController.java
@@ -1,16 +1,19 @@
 package com.group100.VotingApp.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HomeController {
-	
-	@GetMapping("/")
-	public ModelAndView home() {
-		ModelAndView view = new ModelAndView();
-		view.setViewName("candidate");
-	    return view;
+
+
+	@RequestMapping("/")
+	public ModelAndView index () {
+	    ModelAndView modelAndView = new ModelAndView();
+	    modelAndView.setViewName("index");
+	    return modelAndView;
+
 	}
 }
