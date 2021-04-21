ca523d77cea0eab9b52649d680fe3151e81c626e
asmuscma <asmuscma@gmail.com>
2021-04-21 15:44:06 -0500

    Update LoginController.java

--- src/main/java/com/group100/VotingApp/controllers/LoginController.java
+++ src/main/java/com/group100/VotingApp/controllers/LoginController.java
@@ -1,18 +1,20 @@
 package com.group100.VotingApp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class LoginController {
 	
-	/*
-	 * this doesn't work
-	 * 
-	 * @RequestMapping("/login")
-	public ModelAndView login() {
-		ModelAndView model = new ModelAndView();
-		model.setViewName("login");
-		return model;
+	@RequestMapping("/login.html")
+	public String login() {
+		return "login.html";
 	}
-	*/
+	
+	@RequestMapping("/login.html?error")
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		return "login.html";
+	}
 }
