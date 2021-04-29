97008a20ec2f37fa855738d4be073c386510dc6e
PopcornTheBearCat <54016398+PopcornTheBearCat@users.noreply.github.com>
2021-04-29 14:05:04 -0500

    Survey and ballot

--- src/main/java/com/group100/VotingApp/controllers/BallotController.java
+++ src/main/java/com/group100/VotingApp/controllers/BallotController.java
@@ -25,7 +25,7 @@ public class BallotController {
 
 	@Autowired
 	private BallotRepository ballotRepo;
-	
+
 	@Autowired
 	private BallotServiceImp ballotService;
 
@@ -41,28 +41,85 @@ public class BallotController {
 
 	@PostMapping("/add")
 	public Ballot create(@RequestBody final Ballot ballot) {
-		if(!ballotService.checkIfVoted(ballot.getUser().getUsername())) {
+		if (!ballotService.checkIfVoted(ballot.getUser().getUsername())) {
 			return ballotRepo.saveAndFlush(ballot);
 		}
 		return null;
 	}
-	
-	@GetMapping("/ballot")
-	public String ballot(Model model) {
+
+	@GetMapping("/makeballot00")
+	public String ballot00(Model model) {
 		BallotCreationDto ballotsForm = new BallotCreationDto();
-		
-		//TODO: set the range to number of ballot questions
-		for(int i = 0; i <= 3; i++) {
+
+		// TODO: set the range to number of ballot questions
+		for (int i = 0; i <= 3; i++) {
 			ballotsForm.addBallot(new Ballot());
 		}
 		model.addAttribute("form", ballotsForm);
-		return "ballot";
+		return "ballot00";
 	}
-	
-	@PostMapping("/sendballot")
-	public String sendballot(@Valid @ModelAttribute BallotCreationDto form, Model model) {
+
+	@PostMapping("/makeballot00")
+	public String sendballot00(@Valid @ModelAttribute BallotCreationDto form, Model model) {
 		ballotService.saveAll(form.getBallots());
 		model.addAttribute("ballots", ballotService.findAll());
-		return "BallotResults";
+		return "ballot01";
+	}
+
+	@GetMapping("/makeballot01")
+	public String ballot01(Model model) {
+		BallotCreationDto ballotsForm = new BallotCreationDto();
+
+		// TODO: set the range to number of ballot questions
+		for (int i = 0; i <= 3; i++) {
+			ballotsForm.addBallot(new Ballot());
+		}
+		model.addAttribute("form", ballotsForm);
+		return "ballot01";
+	}
+
+	@PostMapping("/makeballot01")
+	public String sendballot01(@Valid @ModelAttribute BallotCreationDto form, Model model) {
+		ballotService.saveAll(form.getBallots());
+		model.addAttribute("ballots", ballotService.findAll());
+		return "ballot02";
+	}
+
+	@GetMapping("/makeballot02")
+	public String ballot02(Model model) {
+		BallotCreationDto ballotsForm = new BallotCreationDto();
+
+		// TODO: set the range to number of ballot questions
+		for (int i = 0; i <= 3; i++) {
+			ballotsForm.addBallot(new Ballot());
+		}
+		model.addAttribute("form", ballotsForm);
+		return "ballot02";
+	}
+
+	@PostMapping("/makeballot02")
+	public String sendballot02(@Valid @ModelAttribute BallotCreationDto form, Model model) {
+		ballotService.saveAll(form.getBallots());
+		model.addAttribute("ballots", ballotService.findAll());
+		return "ballot03";
+	}
+
+	@GetMapping("/makeballot03")
+	public String ballot03(Model model) {
+		BallotCreationDto ballotsForm = new BallotCreationDto();
+
+		// TODO: set the range to number of ballot questions
+		for (int i = 0; i <= 3; i++) {
+			ballotsForm.addBallot(new Ballot());
+		}
+		model.addAttribute("form", ballotsForm);
+		return "ballot03";
+	}
+
+	@PostMapping("/makeballot03")
+	public String sendballot03(@Valid @ModelAttribute BallotCreationDto form, Model model) {
+		ballotService.saveAll(form.getBallots());
+		model.addAttribute("ballots", ballotService.findAll());
+		return "ballot04";
 	}
 }
--- src/main/java/com/group100/VotingApp/controllers/SurveyController.java
+++ src/main/java/com/group100/VotingApp/controllers/SurveyController.java
@@ -8,13 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.group100.VotingApp.data.entity.Survey;
-import com.group100.VotingApp.data.entity.SurveyCreationDto;
 import com.group100.VotingApp.data.repository.SurveyRepository;
 import com.group100.VotingApp.serviceImp.SurveyServiceImp;
 
@@ -60,6 +58,7 @@ public class SurveyController {
 		model.addAttribute("survey", survey);
 		return "survey01";
 	}
+
 	@GetMapping("/survey1")
 	public String addsurvey1(Model model) {
 		return "survey01";
@@ -71,6 +70,7 @@ public class SurveyController {
 		model.addAttribute("survey", survey);
 		return "survey02";
 	}
+
 	@GetMapping("/survey2")
 	public String addsurvey2(Model model) {
 		return "survey02";
@@ -82,100 +82,103 @@ public class SurveyController {
 		model.addAttribute("survey", survey);
 		return "survey03";
 	}
+
 	@GetMapping("/survey3")
 	public String addsurvey3(Model model) {
-		return "survey04";
+		return "survey03";
 	}
 
 	@PostMapping("/survey3")
 	public String savesurvey3(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey05";
+		return "survey04";
 	}
+
 	@GetMapping("/survey4")
 	public String addsurvey4(Model model) {
-		return "survey05";
+		return "survey04";
 	}
 
 	@PostMapping("/survey4")
 	public String savesurvey4(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey06";
+		return "survey05";
 	}
-	
+
 	@GetMapping("/survey5")
 	public String addsurvey5(Model model) {
-		return "survey06";
+		return "survey05";
 	}
 
 	@PostMapping("/survey5")
 	public String savesurvey5(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey07";
+		return "survey06";
 	}
-	
+
 	@GetMapping("/survey6")
 	public String addsurvey6(Model model) {
-		return "survey07";
+		return "survey06";
 	}
 
 	@PostMapping("/survey6")
 	public String savesurvey6(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey08";
+		return "survey07";
 	}
 
 	@GetMapping("/survey7")
 	public String addsurvey7(Model model) {
-		return "survey08";
+		return "survey07";
 	}
 
 	@PostMapping("/survey7")
 	public String savesurvey7(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey09";
+		return "survey08";
 	}
-	
+
 	@GetMapping("/survey8")
 	public String addsurvey8(Model model) {
-		return "survey09";
+		return "survey08";
 	}
 
 	@PostMapping("/survey8")
 	public String savesurvey8(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey10";
+		return "survey9";
 	}
+
 	@GetMapping("/survey9")
 	public String addsurvey9(Model model) {
-		return "survey10";
+		return "survey9";
 	}
 
 	@PostMapping("/survey9")
 	public String savesurvey9(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey11";
+		return "survey10";
 	}
-	
+
 	@GetMapping("/survey10")
 	public String addsurvey10(Model model) {
-		return "survey11";
+		return "survey10";
 	}
 
 	@PostMapping("/survey10")
 	public String savesurvey10(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey12";
+		return "survey11";
 	}
-	
+
 	@GetMapping("/survey11")
 	public String addsurvey11(Model model) {
 		return "survey11";
@@ -185,9 +188,9 @@ public class SurveyController {
 	public String savesurvey11(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey13";
+		return "survey12";
 	}
-	
+
 	@GetMapping("/survey12")
 	public String addsurvey12(Model model) {
 		return "survey12";
@@ -197,110 +200,108 @@ public class SurveyController {
 	public String savesurvey12(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey14";
+		return "survey13";
 	}
-	
+
 	@GetMapping("/survey13")
 	public String addsurvey13(Model model) {
-		return "survey14";
+		return "survey13";
 	}
 
 	@PostMapping("/survey13")
 	public String savesurvey13(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey15";
+		return "survey14";
 	}
-	
+
 	@GetMapping("/survey14")
 	public String addsurvey14(Model model) {
-		return "survey15";
+		return "survey14";
 	}
 
 	@PostMapping("/survey14")
 	public String savesurvey14(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey16";
+		return "survey15";
 	}
-	
+
 	@GetMapping("/survey15")
 	public String addsurvey15(Model model) {
-		return "survey16";
+		return "survey15";
 	}
 
 	@PostMapping("/survey15")
 	public String savesurvey15(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey17";
+		return "survey16";
 	}
-	
+
 	@GetMapping("/survey16")
 	public String addsurvey16(Model model) {
-		return "survey17";
+		return "survey16";
 	}
 
 	@PostMapping("/survey16")
 	public String savesurvey16(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey18";
+		return "survey17";
 	}
-	
-	
+
 	@GetMapping("/survey17")
 	public String addsurvey17(Model model) {
-		return "survey18";
+		return "survey17";
 	}
 
 	@PostMapping("/survey17")
 	public String savesurvey17(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey19";
+		return "survey18";
 	}
-	
+
 	@GetMapping("/survey18")
 	public String addsurvey18(Model model) {
-		return "survey19";
+		return "survey18";
 	}
 
 	@PostMapping("/survey18")
 	public String savesurvey18(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey20";
+		return "survey19";
 	}
-	
+
 	@GetMapping("/survey19")
 	public String addsurvey19(Model model) {
-		return "survey20";
+		return "survey19";
 	}
 
 	@PostMapping("/survey19")
 	public String savesurvey19(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey21";
+		return "survey20";
 	}
-	
-	
+
 	@GetMapping("/survey20")
 	public String addsurvey20(Model model) {
-		return "survey21";
+		return "survey20";
 	}
 
 	@PostMapping("/survey20")
 	public String savesurvey20(@Valid Survey survey, Model model) {
 		surveyRepo.saveAndFlush(survey);
 		model.addAttribute("survey", survey);
-		return "survey22";
+		return "survey21";
 	}
-	
+
 	@GetMapping("/survey21")
 	public String addsurvey21(Model model) {
-		return "survey22";
+		return "survey21";
 	}
 
 	@PostMapping("/survey21")
@@ -309,5 +310,5 @@ public class SurveyController {
 		model.addAttribute("survey", survey);
 		return "result";
 	}
-	
+
 }
