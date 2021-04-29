1ca39b46de87c49eee483e6ff44a7813788afd9a
GoodnewsSama <77777563+GoodnewsSama@users.noreply.github.com>
2021-04-29 13:50:09 -0500

    survey html and controller changes

--- src/main/java/com/group100/VotingApp/controllers/SurveyController.java
+++ src/main/java/com/group100/VotingApp/controllers/SurveyController.java
@@ -49,23 +49,265 @@ public class SurveyController {
 	 * @GetMapping("/createSurvey") public String createSurvey(Model model) {
 	 * model.addAttribute("Survey", new Survey()); return "createSurvey"; }
 	 */
-	@GetMapping("/makesurvey0")
+	@GetMapping("/survey0")
 	public String addsurvey0(Model model) {
-		SurveyCreationDto surveysForm = new SurveyCreationDto();
-		
-		for(int i = 0; i <= 24; i++) {
-			surveysForm.addSurvey(new Survey());
-		}
-		model.addAttribute("form", surveysForm);
 		return "survey0";
 	}
 
-	@PostMapping("/makesurvey0")
-	public String savesurvey0(@Valid @ModelAttribute SurveyCreationDto form, Model model) {
-		/*surveyRepo.saveAndFlush(survey);
-		model.addAttribute("survey", survey);*/
-		surveyService.saveAll(form.getSurveys());
-		model.addAttribute("surveys", surveyService.findAll());
+	@PostMapping("/survey0")
+	public String savesurvey0(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey01";
+	}
+	@GetMapping("/survey1")
+	public String addsurvey1(Model model) {
+		return "survey01";
+	}
+
+	@PostMapping("/survey1")
+	public String savesurvey1(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey02";
+	}
+	@GetMapping("/survey2")
+	public String addsurvey2(Model model) {
+		return "survey02";
+	}
+
+	@PostMapping("/survey2")
+	public String savesurvey2(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey03";
+	}
+	@GetMapping("/survey3")
+	public String addsurvey3(Model model) {
+		return "survey04";
+	}
+
+	@PostMapping("/survey3")
+	public String savesurvey3(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey05";
+	}
+	@GetMapping("/survey4")
+	public String addsurvey4(Model model) {
+		return "survey05";
+	}
+
+	@PostMapping("/survey4")
+	public String savesurvey4(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey06";
+	}
+	
+	@GetMapping("/survey5")
+	public String addsurvey5(Model model) {
+		return "survey06";
+	}
+
+	@PostMapping("/survey5")
+	public String savesurvey5(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey07";
+	}
+	
+	@GetMapping("/survey6")
+	public String addsurvey6(Model model) {
+		return "survey07";
+	}
+
+	@PostMapping("/survey6")
+	public String savesurvey6(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey08";
+	}
+
+	@GetMapping("/survey7")
+	public String addsurvey7(Model model) {
+		return "survey08";
+	}
+
+	@PostMapping("/survey7")
+	public String savesurvey7(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey09";
+	}
+	
+	@GetMapping("/survey8")
+	public String addsurvey8(Model model) {
+		return "survey09";
+	}
+
+	@PostMapping("/survey8")
+	public String savesurvey8(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey10";
+	}
+	@GetMapping("/survey9")
+	public String addsurvey9(Model model) {
+		return "survey10";
+	}
+
+	@PostMapping("/survey9")
+	public String savesurvey9(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey11";
+	}
+	
+	@GetMapping("/survey10")
+	public String addsurvey10(Model model) {
+		return "survey11";
+	}
+
+	@PostMapping("/survey10")
+	public String savesurvey10(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey12";
+	}
+	
+	@GetMapping("/survey11")
+	public String addsurvey11(Model model) {
+		return "survey11";
+	}
+
+	@PostMapping("/survey11")
+	public String savesurvey11(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey13";
+	}
+	
+	@GetMapping("/survey12")
+	public String addsurvey12(Model model) {
+		return "survey12";
+	}
+
+	@PostMapping("/survey12")
+	public String savesurvey12(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey14";
+	}
+	
+	@GetMapping("/survey13")
+	public String addsurvey13(Model model) {
+		return "survey14";
+	}
+
+	@PostMapping("/survey13")
+	public String savesurvey13(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey15";
+	}
+	
+	@GetMapping("/survey14")
+	public String addsurvey14(Model model) {
+		return "survey15";
+	}
+
+	@PostMapping("/survey14")
+	public String savesurvey14(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey16";
+	}
+	
+	@GetMapping("/survey15")
+	public String addsurvey15(Model model) {
+		return "survey16";
+	}
+
+	@PostMapping("/survey15")
+	public String savesurvey15(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey17";
+	}
+	
+	@GetMapping("/survey16")
+	public String addsurvey16(Model model) {
+		return "survey17";
+	}
+
+	@PostMapping("/survey16")
+	public String savesurvey16(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey18";
+	}
+	
+	
+	@GetMapping("/survey17")
+	public String addsurvey17(Model model) {
+		return "survey18";
+	}
+
+	@PostMapping("/survey17")
+	public String savesurvey17(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey19";
+	}
+	
+	@GetMapping("/survey18")
+	public String addsurvey18(Model model) {
+		return "survey19";
+	}
+
+	@PostMapping("/survey18")
+	public String savesurvey18(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey20";
+	}
+	
+	@GetMapping("/survey19")
+	public String addsurvey19(Model model) {
+		return "survey20";
+	}
+
+	@PostMapping("/survey19")
+	public String savesurvey19(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey21";
+	}
+	
+	
+	@GetMapping("/survey20")
+	public String addsurvey20(Model model) {
+		return "survey21";
+	}
+
+	@PostMapping("/survey20")
+	public String savesurvey20(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
+		return "survey22";
+	}
+	
+	@GetMapping("/survey21")
+	public String addsurvey21(Model model) {
+		return "survey22";
+	}
+
+	@PostMapping("/survey21")
+	public String savesurvey21(@Valid Survey survey, Model model) {
+		surveyRepo.saveAndFlush(survey);
+		model.addAttribute("survey", survey);
 		return "result";
 	}
+	
 }
